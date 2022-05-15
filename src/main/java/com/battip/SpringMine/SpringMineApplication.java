ce3510a3f592f89f2764688b159f3487bc2cf68d
battip <battineniprashanth@gmail.com>
2022-05-15 22:28:54 +0900

    JPA Sample
    JPA Sample

--- src/main/java/com/battip/SpringMine/SpringMineApplication.java
+++ src/main/java/com/battip/SpringMine/SpringMineApplication.java
@@ -2,8 +2,11 @@ package com.battip.SpringMine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
+		//(exclude={DataSourceAutoConfiguration.class})
 public class SpringMineApplication {
 
 	public static void main(String[] args) {
